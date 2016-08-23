72b9cb50d09c22ecd13843322ea1e365cecb35fa
Narresh Shetty <Naru@Shettys-MacBook-Pro.local>
2016-08-23 13:44:08 +0530

    added app details in Readme.md

--- ReactApp/app/props.jsx
+++ ReactApp/app/props.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
-   constructor() {
-      super();
-		
-      this.state = {
-         header: "Header from state...",
-         "content": "Content from state..."
-      }
-   }
-	
    render() {
       return (
          <div>
-            <h1>{this.state.header}</h1>
-            <h2>{this.state.content}</h2>
+            <h1>{this.props.headerProp}</h1>
+            <h2>{this.props.contentProp}</h2>
          </div>
       );
    }
 }
 
 export default App;
+
+/*Inside main.js unlike other programs pass values for Header prop and Content so that  props
+can access it inside App Component
+
+ReactDOM.render(<App headerProp = "Header from props..." contentProp = "Content
+   from props..."/>, document.getElementById('app'));*/
