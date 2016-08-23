4fe6d37c283ff07a706b2b30a1c71490d5f56372
Narresh Shetty <Naru@Shettys-MacBook-Pro.local>
2016-08-23 18:28:06 +0530

    Sample Implementation of basic concepts

--- ReactApp/app/main.js
+++ ReactApp/app/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './DefaultProps.jsx';
+import App from './Events.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
